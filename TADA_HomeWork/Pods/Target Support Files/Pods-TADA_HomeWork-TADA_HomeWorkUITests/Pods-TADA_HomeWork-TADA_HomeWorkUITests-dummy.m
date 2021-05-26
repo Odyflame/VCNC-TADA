69148c3c6136536e46243a0408a49d04c9412cbd
hyeonjae Lee <odyflame@icloud.com>
2021-05-27 00:18:38 +0900

    [ Add ] Add 3rd-party libraries
    💁 Description
    Add 3rd libraries
    🎬 Scenario
    install with cocoapod
    🃏 Actual Result
    can use libraries.

---
+++ TADA_HomeWork/Pods/Target Support Files/Pods-TADA_HomeWork-TADA_HomeWorkUITests/Pods-TADA_HomeWork-TADA_HomeWorkUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TADA_HomeWork_TADA_HomeWorkUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TADA_HomeWork_TADA_HomeWorkUITests
+@end
---
+++ TADA_HomeWork/Pods/Target Support Files/Pods-TADA_HomeWorkTests/Pods-TADA_HomeWorkTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TADA_HomeWorkTests : NSObject
+@end
+@implementation PodsDummy_Pods_TADA_HomeWorkTests
+@end
